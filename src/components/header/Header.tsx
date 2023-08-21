ba9e46c5ddc85cdaa241770ec0c075751775fc3f
javierOrtega95 <fjortega@wizzie.io>
2023-08-21 20:24:49 +0200

    feat: add header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,21 @@
+import { Outlet, useNavigation } from 'react-router'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
 export function Header () {
+  const { state } = useNavigation()
+  const isLoading = state === 'loading'
+  console.log(isLoading)
+
   return (
-    <div className="header">
-      <h1>Podcast</h1>
-    </div>
+    <>
+      <header className='header'>
+        <Link to='/'>
+          <h1>Podcast</h1>
+        </Link>
+        {isLoading && <span>Loading...</span>}
+      </header>
+      <Outlet />
+    </>
   )
 }
