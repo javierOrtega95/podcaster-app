0ca3fc6a1c4bccb85d4190e00ac572ab0c945f59
javierOrtega95 <fjortega@wizzie.io>
2023-08-23 16:00:17 +0200

    feat: badge component

---
+++ src/components/badge/Badge.tsx
@@ -0,0 +1,10 @@
+import './badge.css'
+
+interface Props {
+  children: React.ReactNode
+  style: React.CSSProperties
+}
+
+export function Badge ({ children, style }: Props) {
+  return <span className='badge' style={style}>{children}</span>
+}
